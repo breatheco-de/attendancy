280cd1b78df3b6da69b61cdf5479ab963ec268f1
Orlando <monroyorlando.36@gmail.com>
2020-06-10 17:19:59 -0400

    attendancy percentage fixed

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -61,24 +61,70 @@ const getState = ({ getStore, setStore, getActions }) => {
 							})
 							.then(activities => {
 								// Merge students with their activities
+								let student = {};
 								let stuAct = {}; // {student_id: {day0: unattendance, day1: attendance, ...}}
 								let dailyAvg = {}; // {day0: 89%, day1: 61%, ...}
+								//
+								activities.log.filter(item => item.slug.includes("attendance")).forEach(element => {
+									let days = JSON.parse(element.data).day;
+									if (student[element.user_id] === undefined) {
+										student[element.user_id] = {};
+										student[element.user_id].student_id = element.user_id;
+										student[element.user_id].attendance = 0;
+										student[element.user_id].unattendance = 0;
+										student[element.user_id].days = [];
+										student[element.user_id].totalAttendance = 0;
+										student[element.user_id].dailyAttendance = 0;
+									}
 
-								activities.log.filter(e => e.slug.includes("attendance")).forEach(e => {
-									let day = `day${JSON.parse(e.data).day}`;
+									if (!student[element.user_id].days.includes(days)) {
+										student[element.user_id].attendance += element.slug.includes("attendance")
+											? 1
+											: 0;
+										student[element.user_id].unattendance += element.slug.includes("unattendance")
+											? 1
+											: 0;
+										student[element.user_id].days.push(days);
+										student[element.user_id].totalAttendance =
+											(student[element.user_id].days.length * 100) / 45;
+										student[element.user_id].dailyAttendance =
+											(student[element.user_id].attendance * 100) /
+											(student[element.user_id].attendance +
+												student[element.user_id].unattendance);
+										dailyAvg[day] += element.slug.includes("unattendance") ? 0 : 1;
+									}
+
+									let day = `day${JSON.parse(element.data).day}`;
 									// Create temp obj to store all activities by student id
-									if (stuAct[e.user_id] === undefined) {
-										stuAct[e.user_id] = {};
-										stuAct[e.user_id].avg = 0;
+									if (stuAct[element.user_id] === undefined) {
+										stuAct[element.user_id] = {};
+										stuAct[element.user_id].avg = 0;
 									}
 									if (dailyAvg[day] === undefined) {
 										dailyAvg[day] = 0;
 									}
 									// Inside also store all the activities by creating a day property
-									stuAct[e.user_id][day] = e;
-									stuAct[e.user_id].avg += e.slug.includes("unattendance") ? 0 : 1;
-									dailyAvg[day] += e.slug.includes("unattendance") ? 0 : 1;
+									stuAct[element.user_id][day] = element;
+									stuAct[element.user_id].avg += element.slug.includes("unattendance") ? 0 : 1;
+									dailyAvg[day] += element.slug.includes("unattendance") ? 0 : 1;
 								});
+								// Add cohort assistance into students array
+
+								for (let singleStudent in student) {
+									students.forEach(item => {
+										if (item.attendance_log === undefined) {
+											item.attendance_log = {};
+											item.attendance_log.totalAttendance = 0;
+											(item.attendance_log.dailyAttendance = 0),
+												(item.attendance_log.attendance = 0);
+											item.attendance_log.unattendance = 0;
+											item.attendance_log.days = [];
+										}
+										if (item.id == singleStudent) {
+											item.attendance_log = student[singleStudent];
+										}
+									});
+								}
 								// divide by the number of students to get the avg
 								Object.keys(dailyAvg).map(
 									key => (dailyAvg[key] = (dailyAvg[key] / students.length) * 100)
@@ -93,7 +139,9 @@ const getState = ({ getStore, setStore, getActions }) => {
 								setStore({ students, dailyAvg });
 							});
 
-						props.history.push(`/?cohort_slug=${cohortSlug}&bc_token=${access_token}&assets_token=${assets_token}`);
+						props.history.push(
+							`/?cohort_slug=${cohortSlug}&bc_token=${access_token}&assets_token=${assets_token}`
+						);
 					});
 			},
 			formatNames: data => {
